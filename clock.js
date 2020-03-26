8042be673012ae22266384d87c642d8c4fb88bf1
nam <hzzn74@gmail.com>
2020-03-27 00:10:32 +0900

    get user name done

--- clock.js
+++ clock.js
@@ -1,9 +1,16 @@
 window.onload = function(){
-  const clockwrap = document.querySelector(".clock"),
-        clock = clockwrap.querySelector("h1"),
+  //DOM을 가지고 오는 상수
+  const clockWrap = document.querySelector(".clock"),
+        clock = clockWrap.querySelector("h1"),
         form = document.querySelector(".form"),
-        input = form.querySelector("input");
+        input = form.querySelector("input"),
+        greetings = document.querySelector(".greetings");
 
+  //함수에 필요한 상수값들
+  const userStorage = "currentUser",
+        visibleCN = "visible";
+
+  //현재시간을 가지고 오는 함수
   function getTime(){
     const date = new Date();
     const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
@@ -12,11 +19,39 @@ window.onload = function(){
     clock.innerText = `${hours}:${minutes}:${seconds}`;
   }
 
-  function
+  //사용자 이름을 저장하고 인사말을 출력하는 함수들
+  function saveName(userName){
+    localStorage.setItem(userStorage, userName);
+  }
+  function submitName(){
+    event.preventDefault();
+    const userName = input.value;
+    printGreetings(userName);
+    saveName(userName);
+  }
+
+  function getName(){
+    form.classList.add(visibleCN);
+    form.addEventListener("submit", submitName);
+  }
+  function printGreetings(userName){
+    form.classList.remove(visibleCN);
+    greetings.classList.add(visibleCN);
+    greetings.innerText = `Hi ${userName}`;
+  }
+  function loadUser(){
+    const currentUser = localStorage.getItem(userStorage);
+    if(currentUser === null){
+      getName();
+    }else{
+      printGreetings(currentUser);
+    }
+  }
 
   function init(){
     getTime();
     setInterval(getTime, 1000);
+    loadUser();
   }
   init();
 }
